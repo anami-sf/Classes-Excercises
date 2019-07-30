5c8adebc0e801a71548e8867e3264712504d5a30
anami823 <anami823@gmail.com>
2019-07-30 13:21:51 -0700

    Build 'Vehicle' class

--- classes.js
+++ classes.js
@@ -1,16 +1,29 @@
-var cohort = {
-    id: 'SEI',
-    students: ['Mary', 'Toni', 'Fred'],
-    instructors: ['Susan', 'Phil'],
-    addStudent: function(name) {
-      name = name[0].toUpperCase() + name.substr(1).toLowerCase();
-      this.students.push(name);
-    },
-    pickRandomStudent: function() {
-      var rndIdx = Math.floor(Math.random() * this.students.length);
-      return this.students[rndIdx];
-    }
-  };
+// var cohort = {
+//     id: 'SEI',
+//     students: ['Mary', 'Toni', 'Fred'],
+//     instructors: ['Susan', 'Phil'],
+//     addStudent: function(name) {
+//       name = name[0].toUpperCase() + name.substr(1).toLowerCase();
+//       this.students.push(name);
+//     },
+//     pickRandomStudent: function() {
+//       var rndIdx = Math.floor(Math.random() * this.students.length);
+//       return this.students[rndIdx];
+//     }
+//   };
 
-console.log(cohort.addStudent('Anami'))
-console.log(cohort.students)
+// console.log(cohort.addStudent('Anami'))
+// console.log(cohort.students)
+
+class Vehicle {
+    constructor(vin, make, model){
+        this.vin = vin
+        this.make = make
+        this.model = model
+    }
+}
+
+var plane = new Vehicle('X123Y', 'Boeing')
+var car =  new Vehicle('A1234', 'Toyota', 'Camry')
+console.log(plane)
+console.log(car)
