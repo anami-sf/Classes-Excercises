406d7c2feec5382dcfbb8e54d4aac5bc765980bd
anami823 <anami823@gmail.com>
2019-07-30 14:35:53 -0700

    Extend vehicle class

--- classes.js
+++ classes.js
@@ -15,15 +15,73 @@
 // console.log(cohort.addStudent('Anami'))
 // console.log(cohort.students)
 
-class Vehicle {
-    constructor(vin, make, model){
-        this.vin = vin
-        this.make = make
-        this.model = model
-    }
-}
+// class Vehicle {
+//     constructor(vin, make, model){
+//         this.vin = vin
+//         this.make = make
+//         this.model = model
+//         this.running = false
+//     }
 
-var plane = new Vehicle('X123Y', 'Boeing')
-var car =  new Vehicle('A1234', 'Toyota', 'Camry')
-console.log(plane)
-console.log(car)
+//     Start() {
+//         this.running  = true;
+//         console.log('running...');
+//     }
+
+//     stop() {
+//         this.running = false;
+//         console.log("Stopped")
+//     }
+
+//     toString() {
+//         reuturn 
+//             return 'Vehicle (' + this.vin + ') is a ' +
+//                 this.make + ' model ' + this.model;
+//     }
+//     static about() {
+//         console.log("I\'m the vehicle class!")
+//     }
+// }
+
+
+// var plane = new Vehicle('X123Y', 'Boeing')
+// var car =  new Vehicle('A1234', 'Toyota', 'Camry')
+// console.log(plane)
+// console.log(car)
+// console.log(plane.stop())
+
+// class Plane extends Vehicle {
+//     constructor(vin, make, model, airline) {
+//         super(vin, make, model)
+//         this.airline = airline
+//     };
+   
+//     engageAutoPilot() {
+//         console.log('Look mom, no hands!')
+//     }
+// }
+
+// var spyPlane = new Plane('secret', 'Lockhead', 'SR-71', 'USA')
+// //var car = new Vehicle('A1234', 'Toyota', 'Camry')
+// spyPlane.stop()
+
+// class Automobile extends Vehicle {
+//     constructor(vin, make, model){
+//         super(vin, make, model, numDoors)
+//         this.numDoors = this.numDoors
+//     }
+
+//     honk(){
+//         console.log('Hoooonk!')
+//     }
+// }
+
+// var fastCar = new Automobile('TS123', 'Tesla', "P1", 4)
+// console.log(fastCar)
+// fastCar.honk()
+
+// class Stack extends Array {
+//     get top() {
+//       return this[this.length - 1];
+//     }
+//   }
